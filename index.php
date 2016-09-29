3161ddbc6767660308a5b0b44835c0082455a856
Daniel Bispo <daniel@tmw.com.br>
2016-09-29 15:05:16 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'config/conecta.class.php';
+$cotacao = [];
+
+// Montando form
+require_once 'nav/form.php';
+exit;
---
+++ nav/form.php
@@ -0,0 +1,18 @@
+<?php
+if (! isset($cotacao)) {
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cotação de Preços | Aiha Lâmpadas</title>
+        <link href="nav/estilo.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <form id="cotacao" action="" method="post">
+
+        </form>
+    </body>
+</html>
